44dcb590648a257ec0c7f5c58f1290ff87af4eb4
Matlup Mir <98525637+Matlup45@users.noreply.github.com>
2026-04-28 12:17:34 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,21 @@
 
+
+
+
+
+
+If Exists (select 1 from sys.databases where name = 'DataWarehouse')
+Begin
+  Alter Database Datawarehouse set Single_User with rollbaack immediate;
+  drop database DataWarehouse;
+End;
+-- 
+Create Database DataWarehouse;
+--
+Use DataWarehouse;
+--
+Create Schema bronze;
+--
+create schema silver;
+--
+creatre schema gold;
